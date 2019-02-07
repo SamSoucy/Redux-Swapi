e314a8d691f73f8a9473f65a8ad06a9d4f3df0fe
Samsoucy <newf930@outlook.com>
2019-02-06 19:53:07 -0500

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,42 +1,12 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from "redux";
-// import rootReducer from "./reducers";
-// import thunk from "redux-thunk";
-// import logger from "redux-logger";
-// // needed dependancies
-// // applyMiddleware from redux
-// // thunk from redux-thunk
-// // logger from redux-logger
-// // rootReducer from ./reducers
-
-// const store = createStore(
-//   rootReducer, applyMiddleware(thunk, logger));
-//   /* applyMiddleware goes here */
-
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
-
-
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-
-import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
@@ -44,10 +14,9 @@ import rootReducer from "./reducers";
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer,
+  rootReducer, applyMiddleware(thunk, logger));
   /* applyMiddleware goes here */
-  applyMiddleware(thunk, logger)
-);
+
 
 ReactDOM.render(
   <Provider store={store}>
@@ -55,3 +24,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+
